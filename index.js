1b6853cb7a58df55dd5eaee55e94978ffe179c71
rjo5364 <rjo5364@psu.edu>
2025-02-21 20:28:52 -0800

    Updated deploy.yml with env pointers, added KEY for gh envs due to gh policy

--- index.js
+++ index.js
@@ -185,8 +185,8 @@ passport.use(
 passport.use(
   new GitHubStrategy(
     {
-      clientID: process.env.GITHUB_CLIENT_ID,
-      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      clientID: process.env.KEY_GITHUB_CLIENT_ID,
+      clientSecret: process.env.KEY_GITHUB_CLIENT_SECRET,
       callbackURL: `${process.env.BASE_URL}/auth/github/callback`,
       scope: ['user:email']
     },
